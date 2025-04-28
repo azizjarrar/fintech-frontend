1523c99469c1a6ded14c550efaae40bf586d7110
aziz jarrar <46700725+azizjarrar@users.noreply.github.com>
2025-04-28 22:23:47 +0100

    hot fix

--- src/components/dashboard/singleapplicationpage.jsx
+++ src/components/dashboard/singleapplicationpage.jsx
@@ -67,7 +67,7 @@ const Singleapplicationpage = () => {
   const handleDownload = async (fileUrl) => {
     const link = document.createElement("a");
     link.href = fileUrl;
-    link.download = fileUrl.split("/").pop(); 
+    link.download = fileUrl.split("/").pop();
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
@@ -84,10 +84,10 @@ const Singleapplicationpage = () => {
     date === "N/A" || !date
       ? "N/A"
       : new Date(date).toLocaleDateString("en-US", {
-          year: "numeric",
-          month: "long",
-          day: "numeric",
-        });
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+      });
   const user = JSON.parse(localStorage.getItem("user")) || null;
   if (loading) {
     return (
@@ -145,21 +145,23 @@ const Singleapplicationpage = () => {
       )}
 
       {/* Stepper Bar */}
-
       {singleApplication?.status !== "rejected" &&
+
+        singleApplication?.status !== "buyer_disapproved" &&
+
+
         singleApplication?.status !== "rejected_by_madad" && (
           <div className="w-full bg-white h-auto flex flex-col mx-auto justify-between items-center min-h-custom relative">
             <ul className="w-full flex items-center justify-around p-4 relative ">
               <li className="flex items-center justify-around ">
                 <div className="w-[20%] flex items-center justify-around flex-col">
                   <div
-                    className={`w-8 h-8 z-10 bg-background-light flex items-center justify-between border border text-center rounded-full ${
-                      step === 1
+                    className={`w-8 h-8 z-10 bg-background-light flex items-center justify-between border border text-center rounded-full ${step === 1
                         ? "border !bg-primary-default text-white rounded-full flex items-center justify-around"
                         : step > 1
-                        ? "border !bg-primary-light text-primary-dark flex items-center justify-around"
-                        : "border border-black flex items-center justify-around"
-                    }`}
+                          ? "border !bg-primary-light text-primary-dark flex items-center justify-around"
+                          : "border border-black flex items-center justify-around"
+                      }`}
                   >
                     {step >= 1 ? (
                       <svg
@@ -186,20 +188,18 @@ const Singleapplicationpage = () => {
 
               <li className="w-1/5 flex items-center">
                 <div
-                  className={`w-[80%] h-0.5 ${
-                    step >= 2 ? "bg-primary-default" : "bg-background-light"
-                  }`}
+                  className={`w-[80%] h-0.5 ${step >= 2 ? "bg-primary-default" : "bg-background-light"
+                    }`}
                 ></div>
 
                 <div className="w-[20%] flex items-center justify-around flex-col">
                   <div
-                    className={`w-8 h-8 z-10 bg-background-light flex items-center justify-between border border rounded-full ${
-                      step === 2
+                    className={`w-8 h-8 z-10 bg-background-light flex items-center justify-between border border rounded-full ${step === 2
                         ? "border !bg-primary-default text-white rounded-full flex items-center justify-around"
                         : step > 2
-                        ? "border !bg-primary-light text-primary-dark flex items-center justify-around"
-                        : "border border-black flex items-center justify-around"
-                    }`}
+                          ? "border !bg-primary-light text-primary-dark flex items-center justify-around"
+                          : "border border-black flex items-center justify-around"
+                      }`}
                   >
                     {step >= 2 ? (
                       <svg
@@ -226,20 +226,18 @@ const Singleapplicationpage = () => {
 
               <li className="w-1/5 flex items-center">
                 <div
-                  className={`w-[80%] h-0.5 ${
-                    step >= 3 ? "bg-primary-default" : "bg-background-light"
-                  }`}
+                  className={`w-[80%] h-0.5 ${step >= 3 ? "bg-primary-default" : "bg-background-light"
+                    }`}
                 ></div>
 
                 <div className="w-[20%] flex items-center justify-around flex-col">
                   <div
-                    className={`w-8 h-8 z-10 bg-background-light flex items-center justify-between border border rounded-full ${
-                      step === 3
+                    className={`w-8 h-8 z-10 bg-background-light flex items-center justify-between border border rounded-full ${step === 3
                         ? "border !bg-primary-default text-white rounded-full flex items-center justify-around"
                         : step > 3
-                        ? "border !bg-primary-light text-primary-dark flex items-center justify-around"
-                        : "border border-black flex items-center justify-around"
-                    }`}
+                          ? "border !bg-primary-light text-primary-dark flex items-center justify-around"
+                          : "border border-black flex items-center justify-around"
+                      }`}
                   >
                     {step >= 3 ? (
                       <svg
@@ -267,20 +265,18 @@ const Singleapplicationpage = () => {
               {/* Step 4 - Invoice Uploaded */}
               <li className="w-1/5 flex items-center">
                 <div
-                  className={`w-[80%] h-0.5 ${
-                    step >= 4 ? "bg-primary-default" : "bg-background-light"
-                  }`}
+                  className={`w-[80%] h-0.5 ${step >= 4 ? "bg-primary-default" : "bg-background-light"
+                    }`}
                 ></div>
 
                 <div className="w-[20%] flex items-center justify-around flex-col">
                   <div
-                    className={`w-8 h-8 z-10 bg-background-light flex items-center justify-between border border rounded-full ${
-                      step === 4
+                    className={`w-8 h-8 z-10 bg-background-light flex items-center justify-between border border rounded-full ${step === 4
                         ? "border !bg-primary-default text-white rounded-full flex items-center justify-around"
                         : step > 4
-                        ? "border !bg-primary-light text-primary-dark flex items-center justify-around"
-                        : "border border-black flex items-center justify-around"
-                    }`}
+                          ? "border !bg-primary-light text-primary-dark flex items-center justify-around"
+                          : "border border-black flex items-center justify-around"
+                      }`}
                   >
                     {step >= 4 ? (
                       <svg
@@ -308,20 +304,18 @@ const Singleapplicationpage = () => {
               {/* Step 5 - Buyer Approved / Disapproved */}
               <li className="w-1/5 flex items-center">
                 <div
-                  className={`w-[80%] h-0.5 ${
-                    step >= 5 ? "bg-primary-default" : "bg-background-light"
-                  }`}
+                  className={`w-[80%] h-0.5 ${step >= 5 ? "bg-primary-default" : "bg-background-light"
+                    }`}
                 ></div>
 
                 <div className="w-[20%] flex items-center justify-around flex-col">
                   <div
-                    className={`w-8 h-8 z-10 bg-background-light flex items-center justify-between border border rounded-full ${
-                      step === 5
+                    className={`w-8 h-8 z-10 bg-background-light flex items-center justify-between border border rounded-full ${step === 5
                         ? "border !bg-primary-default text-white rounded-full flex items-center justify-around"
                         : step > 5
-                        ? "border !bg-primary-light text-primary-dark flex items-center justify-around"
-                        : "border border-black flex items-center justify-around"
-                    }`}
+                          ? "border !bg-primary-light text-primary-dark flex items-center justify-around"
+                          : "border border-black flex items-center justify-around"
+                      }`}
                   >
                     {step >= 5 ? (
                       <svg
@@ -349,20 +343,18 @@ const Singleapplicationpage = () => {
               {/* Step 6 - Invoice Funded */}
               <li className="w-1/5 flex items-center">
                 <div
-                  className={`w-[80%] h-0.5 ${
-                    step >= 6 ? "bg-primary-default" : "bg-background-light"
-                  }`}
+                  className={`w-[80%] h-0.5 ${step >= 6 ? "bg-primary-default" : "bg-background-light"
+                    }`}
                 ></div>
 
                 <div className="w-[20%] flex items-center justify-around flex-col">
                   <div
-                    className={`w-8 h-8 z-10 bg-background-light flex items-center justify-between border border rounded-full ${
-                      step === 6
+                    className={`w-8 h-8 z-10 bg-background-light flex items-center justify-between border border rounded-full ${step === 6
                         ? "border !bg-primary-default text-white rounded-full flex items-center justify-around"
                         : step > 6
-                        ? "border !bg-primary-light text-primary-dark flex items-center justify-around"
-                        : "border border-black flex items-center justify-around"
-                    }`}
+                          ? "border !bg-primary-light text-primary-dark flex items-center justify-around"
+                          : "border border-black flex items-center justify-around"
+                      }`}
                   >
                     {step >= 6 ? (
                       <svg
@@ -390,20 +382,18 @@ const Singleapplicationpage = () => {
               {/* Step 7 - Closed */}
               <li className="w-1/5 flex items-center">
                 <div
-                  className={`w-[80%] h-0.5 ${
-                    step >= 7 ? "bg-primary-default" : "bg-background-light"
-                  }`}
+                  className={`w-[80%] h-0.5 ${step >= 7 ? "bg-primary-default" : "bg-background-light"
+                    }`}
                 ></div>
 
                 <div className="w-[20%] flex items-center justify-around flex-col">
                   <div
-                    className={`w-8 h-8 z-10 bg-background-light flex items-center justify-between border border rounded-full ${
-                      step === 7
+                    className={`w-8 h-8 z-10 bg-background-light flex items-center justify-between border border rounded-full ${step === 7
                         ? "border !bg-primary-default text-white rounded-full flex items-center justify-around"
                         : step > 7
-                        ? "border !bg-primary-light text-primary-dark flex items-center justify-around"
-                        : "border border-black flex items-center justify-around"
-                    }`}
+                          ? "border !bg-primary-light text-primary-dark flex items-center justify-around"
+                          : "border border-black flex items-center justify-around"
+                      }`}
                   >
                     {step >= 7 ? (
                       <svg
@@ -565,7 +555,7 @@ const Singleapplicationpage = () => {
                   {singleApplication?.uploadedInvoice && (
                     <div className="flex items-center justify-between w-full  py-2">
                       <div className="w-[50%] text-center truncate text-xs font-medium">
-                      Invoice
+                        Invoice
                       </div>
 
                       <div className="w-[50%] flex justify-center">
@@ -607,39 +597,39 @@ const Singleapplicationpage = () => {
 
             <div className="text-black flex flex-col w-full p-2">
               {Object.entries(documents)
-               .filter(([_, value]) => value !== "N/A") 
-              .map(([key, value]) => (
-                <div
-                  key={key}
-                  className="flex justify-between items-center py-2 border-b last:border-b-0"
-                >
-                  <div className="w-[50%] text-center font-semibold text-xs truncate">
-                  {documentMapping[key]}
+                .filter(([_, value]) => value !== "N/A")
+                .map(([key, value]) => (
+                  <div
+                    key={key}
+                    className="flex justify-between items-center py-2 border-b last:border-b-0"
+                  >
+                    <div className="w-[50%] text-center font-semibold text-xs truncate">
+                      {documentMapping[key]}
 
-                  </div>
+                    </div>
 
-                  <div className="w-[50%] text-center flex justify-center p-2">
-                    <button
-                      onClick={() => handleDownload(value)}
-                      className="cursor-pointer hover:bg-primary-dark hover:text-white transition border border-primary-dark rounded w-fit flex p-1 text-primary-dark text-xs items-center justify-around font-semibold"
-                    >
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 24 24"
-                        fill="currentColor"
-                        className="size-5 mr-1"
+                    <div className="w-[50%] text-center flex justify-center p-2">
+                      <button
+                        onClick={() => handleDownload(value)}
+                        className="cursor-pointer hover:bg-primary-dark hover:text-white transition border border-primary-dark rounded w-fit flex p-1 text-primary-dark text-xs items-center justify-around font-semibold"
                       >
-                        <path
-                          fillRule="evenodd"
-                          d="M19.5 21a3 3 0 0 0 3-3V9a3 3 0 0 0-3-3h-5.379a.75.75 0 0 1-.53-.22L11.47 3.66A2.25 2.25 0 0 0 9.879 3H4.5a3 3 0 0 0-3 3v12a3 3 0 0 0 3 3h15Zm-6.75-10.5a.75.75 0 0 0-1.5 0v4.19l-1.72-1.72a.75.75 0 0 0-1.06 1.06l3 3a.75.75 0 0 0 1.06 0l3-3a.75.75 0 1 0-1.06-1.06l-1.72 1.72V10.5Z"
-                          clipRule="evenodd"
-                        />
-                      </svg>
-                      <span>Download</span>
-                    </button>
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 24 24"
+                          fill="currentColor"
+                          className="size-5 mr-1"
+                        >
+                          <path
+                            fillRule="evenodd"
+                            d="M19.5 21a3 3 0 0 0 3-3V9a3 3 0 0 0-3-3h-5.379a.75.75 0 0 1-.53-.22L11.47 3.66A2.25 2.25 0 0 0 9.879 3H4.5a3 3 0 0 0-3 3v12a3 3 0 0 0 3 3h15Zm-6.75-10.5a.75.75 0 0 0-1.5 0v4.19l-1.72-1.72a.75.75 0 0 0-1.06 1.06l3 3a.75.75 0 0 0 1.06 0l3-3a.75.75 0 1 0-1.06-1.06l-1.72 1.72V10.5Z"
+                            clipRule="evenodd"
+                          />
+                        </svg>
+                        <span>Download</span>
+                      </button>
+                    </div>
                   </div>
-                </div>
-              ))}
+                ))}
             </div>
           </div>
         </div>
